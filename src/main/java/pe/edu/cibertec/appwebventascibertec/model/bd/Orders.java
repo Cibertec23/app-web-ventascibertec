91af8f5a6c6a95dca6b1393a8518e49748ab9a96
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-09-16 20:10:43 -0500

    Model base de datos V2 (BD Ventas)

--- src/main/java/pe/edu/cibertec/appwebventascibertec/model/bd/Orders.java
+++ src/main/java/pe/edu/cibertec/appwebventascibertec/model/bd/Orders.java
@@ -1,4 +1,46 @@
 package pe.edu.cibertec.appwebventascibertec.model.bd;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
 public class Orders {
+
+    @Id
+    private Integer orderid;
+    @Column(name = "orderdate")
+    private Date orderdate;
+    @Column(name = "requireddate")
+    private Date requireddate;
+    @Column(name = "shippeddate")
+    private Date shippeddate;
+    @Column(name = "shipvia")
+    private Integer shipvia;
+    @Column(name = "freight")
+    private Double freight;
+    @Column(name = "shipname")
+    private String shipname;
+    @Column(name = "shipaddress")
+    private String shipaddress;
+    @Column(name = "shipcity")
+    private String shipcity;
+    @Column(name = "shipregion")
+    private String shipregion;
+    @Column(name = "shippostalcode")
+    private String shippostalcode;
+    @Column(name = "shipcountry")
+    private String shipcountry;
+    @ManyToOne
+    @JoinColumn(name = "customerid")
+    private Customers customers;
+    @ManyToOne
+    @JoinColumn(name = "employeeid")
+    private Employees employees;
+
+
 }
