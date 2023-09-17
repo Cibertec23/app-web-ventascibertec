05ab9040a3370fc8b86955a25747181bf437ec43
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-09-16 19:46:05 -0500

    Model base de datos (BD Ventas)

---
+++ src/main/java/pe/edu/cibertec/appwebventascibertec/model/bd/Products.java
@@ -0,0 +1,35 @@
+package pe.edu.cibertec.appwebventascibertec.model.bd;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Products {
+    @Id
+    private Integer productid;
+    @Column(name = "productname")
+    private String productname;
+    @Column(name = "quantityperunit")
+    private String quantityperunit;
+    @Column(name = "unitprice")
+    private Double unitprice;
+    @Column(name = "unitsintstock")
+    private Integer unitsintstock;
+    @Column(name = "unitsonorder")
+    private Integer unitsonorder;
+    @Column(name = "reorderlevel")
+    private Integer reorderlevel;
+    @Column(name = "descontinued")
+    private Boolean descontinued;
+    @ManyToOne
+    @JoinColumn(name = "supplierid")
+    private Suppliers suppliers;
+    @ManyToOne
+    @JoinColumn(name = "categoryid")
+    private Categories categories;
+
+
+}
