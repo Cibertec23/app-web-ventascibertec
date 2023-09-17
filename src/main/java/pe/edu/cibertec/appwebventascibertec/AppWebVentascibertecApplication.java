52b0a012f0f1fb2e7416780d11a7d5bc7c5f9d4f
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-09-16 19:01:29 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/appwebventascibertec/AppWebVentascibertecApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebventascibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebVentascibertecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebVentascibertecApplication.class, args);
+	}
+
+}
