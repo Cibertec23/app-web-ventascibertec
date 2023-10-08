5b48cc209e54ae471377f2eabbbbb2fadd08de72
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-10-07 20:31:15 -0500

    update config security and html registro

--- src/main/java/pe/edu/cibertec/appwebventascibertec/configuration/SecurityConfig.java
+++ src/main/java/pe/edu/cibertec/appwebventascibertec/configuration/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 ).formLogin(
                         login ->
                                 login.loginPage("/auth/login")
-                                        .defaultSuccessUrl("/auth/home")
+                                        .defaultSuccessUrl("/auth/login-success")
                                         .usernameParameter("nomusuario")
                                         .passwordParameter("password")
                 ).authenticationProvider(authenticationProvider());
